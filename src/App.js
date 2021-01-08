bfce8eba58359a8f4c9796cf7935a3da49d54821
MZ <mchlzj@gmail.com>
2021-01-08 22:33:03 +0100

    dürfte nicht gepusht werden

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Code wurde um 16:48 erstellt
+          Edit <code>src/App.js</code> Code wurde um 16:48 erstellt. Dürfte nicht gepusht werden.
         </p>
         <a
           className="App-link"
